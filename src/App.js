7a3ccd48fce96116025beb9ed7d7b585b0494a82
malakadasun <greenpagelocal7@gmail.com>
2020-08-01 13:56:06 +0530

    add frontend componenets

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import EventHome from "./ComponentsAkila/EventHome";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import EventCreate from './ComponentsAkila/EventCreate';
 import DashboardTemp from './Dashboard/DashboardTemp';
+import FeedbackTemp from './ComponentsMalaka/FeedbackTemp';
+import feedforms from './componentsdasun/feedforms';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
       <Route component={EventHome} exact path="/event"></Route>
       <Route component={EventCreate} path="/create"></Route>
       <Route component={DashboardTemp} path="/event-dashboard"></Route>
+      <Route component={FeedbackTemp} path="/feedback"></Route>
+      <Route component={feedforms} path="/forms"></Route>
     </Router>
 
   );
