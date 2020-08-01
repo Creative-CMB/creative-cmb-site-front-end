de3b6fdc4a8225932e263b6293375692cf77541e
akila liyanage <mlakilaliyanage@gmail.com>
2020-08-01 19:47:39 +0530

    Updates

---
+++ precache-manifest.8295d7551c632616651dbaf520a99d2c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cd3a5a530cd0153318e4cff21470365",
+    "url": "/creative-cmb-site-front-end/index.html"
+  },
+  {
+    "revision": "1ecce8ad662845fcd459",
+    "url": "/creative-cmb-site-front-end/static/css/2.f8d74a90.chunk.css"
+  },
+  {
+    "revision": "4e9418408ffeb783e9c7",
+    "url": "/creative-cmb-site-front-end/static/css/main.67b3cd03.chunk.css"
+  },
+  {
+    "revision": "1ecce8ad662845fcd459",
+    "url": "/creative-cmb-site-front-end/static/js/2.e8472ea8.chunk.js"
+  },
+  {
+    "revision": "df0ff7f1950c0ce66849c867097dd021",
+    "url": "/creative-cmb-site-front-end/static/js/2.e8472ea8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e9418408ffeb783e9c7",
+    "url": "/creative-cmb-site-front-end/static/js/main.2e26f3ec.chunk.js"
+  },
+  {
+    "revision": "ed62d4534cb0a86339da",
+    "url": "/creative-cmb-site-front-end/static/js/runtime-main.e16003ab.js"
+  },
+  {
+    "revision": "5568070fe7f9cdb6bb3434d43b4fd074",
+    "url": "/creative-cmb-site-front-end/static/media/logo.5568070f.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/creative-cmb-site-front-end/static/media/logo.ee7cd8ed.svg"
+  }
+]);
