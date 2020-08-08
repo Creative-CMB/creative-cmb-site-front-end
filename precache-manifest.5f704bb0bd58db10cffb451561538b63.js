017137e29a410e7ab3d68db2e9b1c152dfabb9fa
akila liyanage <mlakilaliyanage@gmail.com>
2020-08-08 09:28:37 +0530

    Updates

---
+++ precache-manifest.5f704bb0bd58db10cffb451561538b63.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "287c127744d7303e963e87faa6eab844",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e845256e732e8a08c1b0",
+    "url": "/static/css/2.cfe6160f.chunk.css"
+  },
+  {
+    "revision": "240b48a8290c61117cba",
+    "url": "/static/css/main.a175cfae.chunk.css"
+  },
+  {
+    "revision": "e845256e732e8a08c1b0",
+    "url": "/static/js/2.a1b4d1b1.chunk.js"
+  },
+  {
+    "revision": "bbe2dbbdaea8525fcc46934c5c58f863",
+    "url": "/static/js/2.a1b4d1b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "240b48a8290c61117cba",
+    "url": "/static/js/main.0d77f838.chunk.js"
+  },
+  {
+    "revision": "2f01ded8bcd4d8dd4273",
+    "url": "/static/js/runtime-main.013cb01d.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "bd624301175622896ea03f1599fc1a9e",
+    "url": "/static/media/main.bd624301.png"
+  },
+  {
+    "revision": "7496698a221365d02d5df1b0526d57b7",
+    "url": "/static/media/slide1.7496698a.png"
+  },
+  {
+    "revision": "385adee1807b8ba97c5b25351c7c9022",
+    "url": "/static/media/user.385adee1.png"
+  }
+]);
