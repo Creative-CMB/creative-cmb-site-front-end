979e02ec97e03ed7ecc4d87c6e9ed83373cfeb47
himashax <himasha.mandakini@gmail.com>
2020-08-22 14:44:30 +0530

    tch whatever

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import EventHome from "./ComponentsAkila/EventHome";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import EventCreate from './ComponentsAkila/EventCreate';
 import EventEditHome from './ComponentsAkila/EventEditHome';
+import InventoryItems from './ComponentsHimasha/InventoryItems';
+import RentalDetails from './ComponentsHimasha/RentalDetails';
+import AddItems from './ComponentsHimasha/AddItems';
 
 function App() {
   return (
@@ -13,6 +16,8 @@ function App() {
       <Route component={EventHome} exact path="/event"></Route>
       <Route component={EventCreate} path="/create"></Route>
       <Route component={EventEditHome} path="/edit"></Route>
+      <Route component={InventoryItems} path= "/inventory"></Route>
+      <Route component={AddItems} path= "/add"></Route>
     </Router>
 
   );
