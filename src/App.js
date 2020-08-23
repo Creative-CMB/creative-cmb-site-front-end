dd577293a567455b6c36f0c2a5dd212400488181
akila liyanage <mlakilaliyanage@gmail.com>
2020-08-23 09:00:39 +0530

    dashboard template

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from "react";
 import logo from "./logo.svg";
+import "./assets/scss/style.css";
 import "./App.css";
 import "./ComponentsAkila/EventHome";
 import EventHome from "./ComponentsAkila/EventHome";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import indexRoutes from "./routes/index"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  HashRouter,
+} from "react-router-dom";
 import EventCreate from "./ComponentsAkila/EventCreate";
 import EventEditHome from "./ComponentsAkila/EventEditHome";
 import EventSuccess from "./ComponentsAkila/EventSuccess";
 import Feedback from './ComponentsMalaka/FeedbackTemp';
 import Adminfeedback from './ComponentsMalaka/feedadmin';
 import Editfeedbac from './ComponentsMalaka/editfeedback';
+
 
 
 
@@ -22,7 +32,15 @@ function App() {
       <Route component={Feedback} path="/feedback"></Route>
       <Route component={Adminfeedback} path="/adminfeedback"></Route>
       <Route component={Editfeedbac} path="/editfeedback"></Route>
-
+      <HashRouter>
+        <Switch>
+          {indexRoutes.map((prop, key) => {
+            return (
+              <Route path={prop.path} key={key} component={prop.component} />
+            );
+          })}
+        </Switch>
+      </HashRouter>
     </Router>
   );
 }
